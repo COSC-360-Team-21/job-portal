a9e4e02b18b2c13fbc86fb58194a41178fc92bd9
alepiman <aliffmlg@gmail.com>
2026-03-17 16:17:39 -0700

    fix: correct indentation in server.js for better readability

--- backend/server.js
+++ backend/server.js
@@ -5,5 +5,5 @@ import connectDB from './src/config/db.js';
 const PORT = process.env.PORT || 5000;
 
 connectDB().then(() => {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
